91952842a8b5a054660aa3c95a3e77c166f0aa05
KonstantinKhamilov <134136455+KonstantinKhamilov@users.noreply.github.com>
2024-05-18 15:53:08 +0000

    Deploying to gh-pages from @ KonstantinKhamilov/goit-js-hw-12@8dac2bfa20f4884d9288ade07af51579c43d71d5 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,12 +1,12 @@
-import{a as u,S as p,i as f}from"./assets/vendor-09d7c26e.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&o(i)}).observe(document,{childList:!0,subtree:!0});function n(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(e){if(e.ep)return;e.ep=!0;const r=n(e);fetch(e.href,r)}})();const m="43943872-0cd26c97fed3a84845e31fcef";function h(s){return u.get(`https://pixabay.com/api/?key=${m}&q=${s}&image_type=photo&orientation=horizontal&safesearch=true`).then(t=>t.data.hits).catch(t=>console.error(t))}function y(s,t){if(s.length===0){iziToast.error({title:"Error",message:"Изображения, соответствующие вашему запросу, не найдены. Пожалуйста, попробуйте еще раз."});return}const n=s.map(o=>`
-    <div class="image-card">
-      <a href="${o.largeImageURL}"><img src="${o.webformatURL}" alt="${o.tags}" title=""/></a>
-      <div class="image-info">
-        <p><span>Просмотры: </span>${o.views}</p>
-        <p><span>Загрузки: </span>${o.downloads}</p>
-        <p><span>Лайки: </span>${o.likes}</p>
-        <p><span>Комментарии: </span>${o.comments}</p>
-      </div>
-    </div>
-  `).join("");t.innerHTML=n}const g=document.querySelector("#search-form"),a=document.querySelector("#search-input"),l=document.querySelector("#gallery"),c=document.querySelector("#loader");let d;g.addEventListener("submit",s=>{s.preventDefault();const t=a.value.trim();if(!t){alert("Пожалуйста, введите ключевое слово для поиска");return}c.classList.remove("hidden"),h(t).then(n=>{c.classList.add("hidden"),y(n,l),d=new p(".image-card a",{}),a.value=""}).catch(n=>{console.error(n),f.error({title:"Error",message:"Произошла ошибка при загрузке изображений. Пожалуйста, попробуйте еще раз."})})});l.addEventListener("click",s=>{s.target.nodeName==="IMG"&&d.show()});
+import{a as m,S as h,i as l}from"./assets/vendor-09d7c26e.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const c of r.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&o(c)}).observe(document,{childList:!0,subtree:!0});function n(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function o(e){if(e.ep)return;e.ep=!0;const r=n(e);fetch(e.href,r)}})();const y="43943872-0cd26c97fed3a84845e31fcef";function g(t,s){return m.get(`https://pixabay.com/api/?key=${y}&q=${t}&image_type=photo&orientation=horizontal&safesearch=true&page=${s}&per_page=15`).then(n=>n.data.hits).catch(n=>console.error(n))}function L(t,s){if(t.length===0){iziToast.error({title:"Error",message:"Изображения, соответствующие вашему запросу, не найдены. Пожалуйста, попробуйте еще раз."});return}const n=t.map(o=>`
+      <li class="image-card">
+  <a href="${o.largeImageURL}"><img src="${o.webformatURL}" alt="${o.tags}" title="" style="height:150px; width:150px;"/></a>
+  <div class="image-info">
+    <p><span>Просмотры: </span>${o.views}</p>
+    <p><span>Загрузки: </span>${o.downloads}</p>
+    <p><span>Лайки: </span>${o.likes}</p>
+    <p><span>Комментарии: </span>${o.comments}</p>
+  </div>
+</li>
+  `).join("");s.innerHTML+=n}const v=document.querySelector("#search-form"),$=document.querySelector("#search-input"),u=document.querySelector("#gallery"),d=document.querySelector("#loader"),i=document.querySelector("#load-more");let p=1,a="";v.addEventListener("submit",t=>{if(t.preventDefault(),a=$.value.trim(),!a){alert("Пожалуйста, введите ключевое слово для поиска");return}p=1,u.innerHTML="",f()});i.addEventListener("click",f);function f(){d.classList.remove("hidden"),g(a,p++).then(t=>{console.log(t),d.classList.add("hidden"),L(t,u),new h(".image-card a",{}),t.length===15?i.style.display="block":(i.style.display="none",l.info({title:"Info",message:"We're sorry, but you've reached the end of search results."}))}).catch(t=>{console.error(t),l.error({title:"Error",message:"Произошла ошибка при загрузке изображений. Пожалуйста, попробуйте еще раз."})}),scrollToNextPage()}
 //# sourceMappingURL=commonHelpers.js.map
